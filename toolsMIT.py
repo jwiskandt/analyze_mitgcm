09b4ed3d4a1f7f1c5f77bea4d60b7aad9542ecb7
jonathan <jonathan.wiskandt@misu.su.se>
2022-04-04 16:28:50 +0200

    add load_path to tools

--- toolsMIT.py
+++ toolsMIT.py
@@ -3,6 +3,41 @@ import pickle
 from matplotlib import cm
 
 
+def load_path():
+    return [
+        "2lay_1SLOPE",
+        "2lay_1SLOPE_NUe-4",
+        "2lay_1SLOPE_NUe-5",
+        "2lay_1SLOPE_nAW25",
+        "2lay_1SLOPE_nAW20",
+        "2lay_1SLOPE_nAW15",
+        "2lay_1SLOPE_nAW10",
+        "2lay_1SLOPE_nAW05",
+        "2lay_1SLOPE_AW00",
+        "2lay_1SLOPE_AW10",
+        "2lay_1SLOPE_AW15",
+        "2lay_1SLOPE_AW20",
+        "2lay_1SLOPE_AW25",
+        "2lay_1SLOPE_AW30",
+        "2lay_1SLOPE_AW35",
+        "2lay_1SLOPE_AW40",
+        "2lay_1SLOPE_AW45",
+        "2lay_1SLOPE_AW50",
+        "2lay_1SLOPE_AW55",
+        "2lay_1SLOPE_AW60",
+        "sgd_2lay_1SLOPE_nAW20",
+        "sgd_2lay_1SLOPE_nAW10",
+        "sgd_2lay_1SLOPE_AW00",
+        "sgd_2lay_1SLOPE",
+        "sgd_2lay_1SLOPE_AW10",
+        "sgd_2lay_1SLOPE_AW20",
+        "sgd_2lay_1SLOPE_AW30",
+        "sgd_2lay_1SLOPE_AW40",
+        "sgd_2lay_1SLOPE_AW50",
+        "sgd_2lay_1SLOPE_AW60",
+    ]
+
+
 def short_path(path):
     for i in np.arange(0, len(path)):
         if len(path[i]) < 12:
