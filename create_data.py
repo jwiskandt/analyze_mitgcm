74a43128adb8b42982d18aa101e11e24674654ba
jonathan <jonathan.wiskandt@misu.su.se>
2022-06-13 09:23:17 +0200

    correction

--- create_data.py
+++ create_data.py
@@ -32,10 +32,10 @@ def timeseries(path, start, stop, step, which=[], freq=360):
 def timeave(path, start, stop, step, which=[], freq=[]):
     range = stop - start
     for i in np.arange(0, np.shape(path)[0]):
-        varfile = "mydata/" + path[i] + range + "/var_final.pkl"
-        coordsfile = "mydata/" + path[i] + range + "/coords.pkl"
-        SHIflxfile = "mydata/" + path[i] + range + "/SHIflx_final.pkl"
-        gammafile = "mydata/" + path[i] + range + "/gamma_final.pkl"
+        varfile = "mydata/" + path[i] + str(range) + "/var_final.pkl"
+        coordsfile = "mydata/" + path[i] + str(range) + "/coords.pkl"
+        SHIflxfile = "mydata/" + path[i] + str(range) + "/SHIflx_final.pkl"
+        gammafile = "mydata/" + path[i] + str(range) + "/gamma_final.pkl"
         coords = loadMIT.load_coord(path[i])
         with open(coordsfile, "wb") as a_file:
             pickle.dump(coords, a_file)
